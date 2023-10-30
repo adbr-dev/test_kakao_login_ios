bbcd46dfda942159ca5b6dd6dd6fa113c25a8062
adbr <adbr.dev@gmail.com>
2023-10-30 18:36:43 +0900

    feat: login init setting (comeback to app)

--- testKakaoApp/MyAppDelegate.swift
+++ testKakaoApp/MyAppDelegate.swift
@@ -8,7 +8,7 @@
 import Foundation
 import KakaoSDKCommon
 import UIKit
-
+import KakaoSDKAuth
 
 class MyAppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -19,5 +19,13 @@ class MyAppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                return AuthController.handleOpenUrl(url: url)
+            }
+
+            return false
+        }
     
 }
